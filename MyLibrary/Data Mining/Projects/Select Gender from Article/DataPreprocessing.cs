e140a6a32aca98190ccc38d2284db16c6d9b3172
Halil Şahin <xelilim@hotmail.com>
2017-11-25 12:31:50 +0300

    master

---
+++ MyLibrary/Data Mining/Projects/Select Gender from Article/DataPreprocessing.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyLibrary.Data_Mining.Projects.Select_Gender_from_Article
+{
+    class DataPreprocessing
+    {
+        private String text;
+
+        public DataPreprocessing(String text)
+        {
+            this.text = text;
+            
+        }
+
+
+        /// <summary>
+        /// Clastaki tüm metodları birleştiren metotdur
+        /// </summary>
+        /// <returns>0-255 sayılarından kaç tane olduğunu bir dizi ile dönderiyor</returns>
+        public float[] Main()
+        {
+            String Metin = Surplus(text);
+
+            Byte[] liste1 = Encoding.ASCII.GetBytes(Metin);
+
+            List<Byte> liste2 = AddZeros(liste1.ToList<Byte>());
+
+            String[] liste3 = CompareToNeighborgs(liste2);
+
+            List<Byte> liste4 = ConvertToDecimal(liste3);
+
+            Byte[] liste5 = liste4.ToArray();
+
+            float[] liste6 = PieceOfNumbers(liste5);
+
+            return liste6;
+        }
+
+
+        /// <summary>
+        /// Metnin İçindeki noktalama işaretleri atar.
+        /// </summary>
+        /// <param name="text">Gelen metin</param>
+        /// <returns>Noktalama işaretleri atılmış hali</returns>
+        private String Surplus(String text)
+        {
+            Char[] karakter = { ' ', '?', '.', ',', ';', ':', '!', '-', '*', '<', '>' };
+            String ss = "";
+            String[] abc = text.Split(karakter);
+            foreach (String item in abc)
+            {
+                ss += item;
+            }
+            return ss;
+        }
+
+
+        /// <summary>
+        /// Listedeki karakterlerin ASCII karşılığı bir listede, listenin sağına ve soluna 4 adet sıfır ekliyor
+        /// </summary>
+        /// <param name="ZerosArray">Karakterlerin ASCII karşılığı bir liste</param>
+        /// <returns>Sağına ve soluna 4 sıfır ekli halini dönderiyor</returns>
+        private List<Byte> AddZeros(List<Byte> ZerosArray)
+        {
+
+            ZerosArray.Insert(0, 0);
+            ZerosArray.Insert(1, 0);
+            ZerosArray.Insert(2, 0);
+            ZerosArray.Insert(3, 0);
+            ZerosArray.Insert(ZerosArray.Count, 0);
+            ZerosArray.Insert(ZerosArray.Count - 1, 0);
+            ZerosArray.Insert(ZerosArray.Count - 2, 0);
+            ZerosArray.Insert(ZerosArray.Count - 3, 0);
+            return ZerosArray;
+        }
+
+
+
+        /// <summary>
+        /// Algoritmaıyı oluşturan metoddur. 4 sağ ve 4 sol kontrol ederek 8 bitlik bir binary kod oluşturuyor
+        /// </summary>
+        /// <param name="CompareList">Sağına ve soluna 4 sıfır ekli halinde gelen bir dizi</param>
+        /// <returns>Oluşturulan 8 bitlik kodları bir dizi şeklinde dönderiyor.</returns>
+        private String[] CompareToNeighborgs(List<Byte> CompareList)
+        {
+            String[] GenuineList = new String[CompareList.Count - 8];
+
+            for (int i = 4; i < CompareList.Count - 4; i++)
+            {
+                for (int j = 1; j < 5; j++)
+                {
+                    if (CompareList[i] >= CompareList[i - j])
+                    {
+                        GenuineList[i - 4] += "1";
+                    }
+                    else if (CompareList[i] < CompareList[i - j])
+                    {
+                        GenuineList[i - 4] += "0";
+                    }
+                }
+
+                for (int s = 1; s < 5; s++)
+                {
+                    if (CompareList[i] >= CompareList[i + s])
+                    {
+                        GenuineList[i - 4] += "1";
+                    }
+                    else if (CompareList[i] < CompareList[i + s])
+                    {
+                        GenuineList[i - 4] += "0";
+                    }
+                }
+
+            }
+            return GenuineList;
+        }
+
+
+
+        /// <summary>
+        /// 8 bitlik binary kodu decimal koda çeviren metoddur.
+        /// </summary>
+        /// <param name="CompareToNeighborgsSecond">8 bitlik kodları barındıran bir dizi</param>
+        /// <returns>8 bitlik kodların karşılığı olan decimal kodları bir liste şeklinde dönderiyor.</returns>
+        private List<Byte> ConvertToDecimal(String[] CompareToNeighborgsSecond)
+        {
+            List<Byte> ConvertToDecimal2 = new List<byte>();
+            byte dec = 0;
+
+            for (int i = 0; i < CompareToNeighborgsSecond.Length; i++)
+            {
+                for (int k = 0; k < CompareToNeighborgsSecond[i].Length; k++)
+                {
+
+                    if ((CompareToNeighborgsSecond[i].Substring(k, 1)).Equals("0"))
+                        continue;
+                    dec += (byte)Math.Pow(2, 7 - k);
+                }
+                ConvertToDecimal2.Add(dec);
+                dec = 0;
+            }
+
+            return ConvertToDecimal2;
+        }
+
+
+
+        /// <summary>
+        /// 0-255 sayılarından kaç adet olduğunu bulan metoddur
+        /// </summary>
+        /// <param name="Piece">decimal kodların bulunduğu dizidir</param>
+        /// <returns>0-255 sayılarından kaç adet olduğunu bulup ve diziyle dönderir.</returns>
+        private float[] PieceOfNumbers(Byte[] Piece)
+        {
+            float[] matris = new float[256];
+
+            for (int i = 0; i < Piece.Length; i++)
+            {
+                for (int k = 0; k < 256; k++)
+                {
+                    if (Piece[i] == k)
+                    {
+                        matris[k]++;
+                    }
+                }
+            }
+            return matris;
+        }
+
+
+    }
+}
---
+++ MyLibrary/Data Mining/Projects/Select Gender from Article/main.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibrary.Data_Mining.Projects.Select_Gender_from_Article
+{
+    class main
+    {
+    }
+}
